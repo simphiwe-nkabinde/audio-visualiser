305501121f1403934c04e7d7e0c431977892e230
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2024-04-26 18:59:53 +0200

    setup for new project: Umsebenzi

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,4 @@
-export const SCREEN_WIDTH = window.innerWidth / 2
-export const SCREEN_HEIGHT = window.innerHeight / 2
+export const SCREEN_WIDTH = window.innerWidth 
+export const SCREEN_HEIGHT = window.innerHeight
 export const FOCAL_LENGTH = 320 //distance from viewer to z=0
 export const TRUE_ORIGIN = { x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2, z: 0 };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import './style.css'
-import CirclesInSpaceVisualiser from './classes/visualisers/CirclesInSpaceVisualiser';
-import BarGraphVisualiser from './classes/visualisers/BarGraphVisualiser';
-import waveformVisualiser from './classes/visualisers/WaveformVisualiser';
-import BubbleChaosVisualiser from './classes/visualisers/BubbleChaosVisualiser';
 import { AudioOptions } from './interfaces/interfaces';
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from './globals';
+import Umsebenzi from './classes/projects/Umsebenzi';
 
 document.querySelector('#start-btn')!.addEventListener('click', (e) => {
     document.querySelector('#overlay')!.remove();
@@ -24,20 +21,10 @@ function visualizer() {
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 
-    const [canvas1, canvas2, canvas3, canvas4] = document.querySelectorAll('canvas')!;
-    canvas1.width = SCREEN_WIDTH;
-    canvas1.height = SCREEN_HEIGHT;
-    canvas2.width = SCREEN_WIDTH;
-    canvas2.height = SCREEN_HEIGHT;
-    canvas3.width = SCREEN_WIDTH;
-    canvas3.height = SCREEN_HEIGHT;
-    canvas4.width = SCREEN_WIDTH;
-    canvas4.height = SCREEN_HEIGHT;
-
-    const canvasCtx1 = canvas1.getContext('2d')!;
-    const canvasCtx2 = canvas2.getContext('2d')!;
-    const canvasCtx3 = canvas3.getContext('2d')!;
-    const canvasCtx4 = canvas4.getContext('2d')!;
+    const canvas= document.querySelector('canvas')!;
+    canvas.width = SCREEN_WIDTH;
+    canvas.height = SCREEN_HEIGHT;
+    const canvasCtx = canvas.getContext('2d')!;
 
     const frequncyDataAudioOptions: AudioOptions = {
         bufferLength,
@@ -45,20 +32,7 @@ function visualizer() {
         getByteDataCallback: (data) => analyser.getByteFrequencyData(data),
         duration: audio.duration
     }
-    const timeDomainDataAudioOptions: AudioOptions = {
-        bufferLength,
-        dataArray,
-        getByteDataCallback: (data) => analyser.getByteTimeDomainData(data),
-        duration: audio.duration
-    }
 
-    const circlesInSpace = new CirclesInSpaceVisualiser(canvasCtx1, frequncyDataAudioOptions);
+    const circlesInSpace = new Umsebenzi(canvasCtx, frequncyDataAudioOptions);
     circlesInSpace.renderVisualisation();
-    const waveForm = new waveformVisualiser(canvasCtx2, timeDomainDataAudioOptions);
-    waveForm.renderVisualisation();
-    const barGraph = new BarGraphVisualiser(canvasCtx3, frequncyDataAudioOptions);
-    barGraph.renderVisualisation();
-    const bubbleChaos = new BubbleChaosVisualiser(canvasCtx4, frequncyDataAudioOptions);
-    bubbleChaos.renderVisualisation();
-
 }
